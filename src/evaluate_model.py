b87f7f059d8f1e2c4309c264b0cf8cf813d76076
roshanchaudhary861 <149452941+roshanchaudhary861@users.noreply.github.com>
2026-04-18 22:07:29 +0100

    confusion matrix added

--- src/evaluate_model.py
+++ src/evaluate_model.py
@@ -1,10 +1,25 @@
-from sklearn.metrics import accuracy_score, classification_report
+from sklearn.metrics import accuracy_score, classification_report, ConfusionMatrixDisplay
+import matplotlib.pyplot as plt
+
 
 def evaluate(models, X_test, y_test):
 
     for name, model in models.items():
+
+        # Predictions
         predictions = model.predict(X_test)
 
+        # Print results
         print(f"\nModel: {name}")
         print("Accuracy:", accuracy_score(y_test, predictions))
         print(classification_report(y_test, predictions))
+
+        # 🔥 Confusion Matrix
+        disp = ConfusionMatrixDisplay.from_predictions(
+            y_test,
+            predictions,
+            cmap="Blues"
+        )
+
+        plt.title(f"{name} - Confusion Matrix")
+        plt.show()
